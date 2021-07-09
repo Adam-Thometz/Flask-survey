dc6857959bc7a4115563629d45420e27a5a03be5
Adam-Thometz <adamthometz@gmail.com>
2021-07-09 09:02:49 -0400

    Survey w/o session

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template, redirect, flash
+from surveys import satisfaction_survey as survey
+from flask_debugtoolbar import DebugToolbarExtension
+
+responses = []
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "hello-world!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def show_start():
+    title = survey.title
+    instructions = survey.instructions
+    return render_template('instructions.html', title = title, instructions = instructions)
+
+@app.route('/begin')
+def begin_survey():
+    return redirect('/question/0')
+
+@app.route('/question/<int:i>')
+def show_question(i):
+    if i != len(responses):
+        flash("I don't think so. >:(")
+        return redirect(f'/question/{len(responses)}')
+    if len(responses) == len(survey.questions):
+        flash("Dude you finished, stop being an over achiever")
+        return redirect('/complete')
+    
+    question = survey.questions[i].question
+    choices = survey.questions[i].choices
+    return render_template('question.html', question = question, choices = choices, i = i+1)
+
+@app.route('/answer', methods=['POST'])
+def add_answer():
+    choice = request.form["answer"]
+    responses.append(choice)
+
+    if len(responses) == len(survey.questions):
+        return redirect('/complete')
+    else:
+        return redirect(f'/question/{len(responses)}')
+
+@app.route('/complete')
+def show_finish():
+    return render_template('complete.html')
+
